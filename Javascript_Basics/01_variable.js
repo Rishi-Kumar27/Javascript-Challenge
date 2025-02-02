063e03520039e4169366a7f5ddf91a0e1267c16f
Rishi Kumar <rishikumar27203@gmail.com>
2025-02-02 21:50:34 +0530

    inital commit

---
+++ Javascript_Basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 12678;
+let accountEmail = "rishi125@google.com";
+var accountPassword = "343678"
+accountCity = "Delhi";
+let accountState;
+
+// accountId = 23145;
+
+console.log(accountId);
+
+accountEmail = "rishik87@gmail.com";
+accountPassword = "12356";
+accountCity = "U.P"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);

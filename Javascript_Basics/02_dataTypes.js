63a5a4a76e053551887c88556ea3f6ee4ccc6f00
Rishi Kumar <rishikumar27203@gmail.com>
2025-02-03 12:26:18 +0530

    dataTypes

---
+++ Javascript_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all JS Code as Newer version
+
+// alert(3 + 3) // We are using Nodejs , not browser
+
+console.log(3 + 3) //Code readability should be high
+
+console.log("Rishi");
+
+let name = "Rishi"
+let age = 19
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);
+

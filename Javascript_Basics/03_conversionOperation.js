99652695284452c650fa2b6b6f67b8e38954f767
Rishi Kumar <rishikumar27203@gmail.com>
2025-02-03 14:49:33 +0530

    conversion of datatypes

---
+++ Javascript_Basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "Rishi"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "rishi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "rishi" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

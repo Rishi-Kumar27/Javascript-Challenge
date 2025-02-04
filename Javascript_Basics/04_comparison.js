058115541f34fb6a57a1b0bd578b07c48b9c276d
Rishi Kumar <rishikumar27203@gmail.com>
2025-02-04 13:23:39 +0530

    comparison datatypes

---
+++ Javascript_Basics/04_comparison.js
@@ -0,0 +1,30 @@
+// *******Simple Comparison*********
+
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// *********Different dataTypes*********
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+//***********NULL*********** 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+//**********Undefined*********
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ==== Strict Equality
+
+console.log("2" === 2);
